ce4cfaf060b5bc1e944cbc239359562387f190ae
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-03-14 14:18:59 +0100

    POC

--- index.php
+++ index.php
@@ -15,13 +15,12 @@
 				var pc = new RTCPeerConnection({iceServers: []}), noop = function () {
 				};
 
-				console.log(pc);
-
 				pc.createDataChannel("");	 //create a bogus data channel
 				pc.createOffer(pc.setLocalDescription.bind(pc), noop);	  // create offer and set local description
 				pc.onicecandidate = function (ice) {  //listen for candidate events
 					if (!ice || !ice.candidate || !ice.candidate.candidate) return;
 					var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+
 					jQuery(document).ready(function () {
 						jQuery("#local-ip").append("<li class=\"list-group-item\">Your Local IP is: " + myIP + "</li>");
 						jQuery("#local-ip").show("slow");
